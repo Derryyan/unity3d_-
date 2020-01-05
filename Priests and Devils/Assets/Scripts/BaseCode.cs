68ec098a30ec4ca6a933a273e049281bd008f139
derry <1754871078@qq.com>
2020-01-05 19:27:21 +0800

    Add files via upload

---
+++ Priests and Devils/Assets/Scripts/BaseCode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PriestsAndDevils;
+
+namespace PriestsAndDevils {
+
+	public class Director : System.Object {
+		private static Director _instance;
+		public SceneController currentSceneController { get; set; }
+
+		public static Director getInstance() {
+			if (_instance == null) {
+				_instance = new Director ();
+			}
+			return _instance;
+		}
+	}
+
+	public interface SceneController {		//加载场景
+		void loadResources ();
+	}
+
+	public interface UserAction {     //动作管理
+		void moveBoat();
+		void characterIsClicked(MyCharacterController characterCtrl);
+		void restart();
+	}
+}
