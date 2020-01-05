b28fcd1dbbbe61c9262fe8de9d368a11f1da53e4
derry <1754871078@qq.com>
2020-01-05 19:32:18 +0800

    Add files via upload

---
+++ hw9HealthBar/Assets/Scripts/IMGUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUI : MonoBehaviour
+{
+    
+	public float showHP;
+	public float HP;
+
+	public Rect HealthBar;
+	public Rect HealthUp;
+	public Rect HealthDown;
+
+	public Slider HealthSlider;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        showHP = 0.1f;
+        HP = 0.1f;
+
+        HealthBar = new Rect(25,25,300,50);
+        HealthUp = new Rect(50,50,50,30);
+        HealthDown = new Rect(100,50,50,30);
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        if (GUI.Button(HealthUp, "加血"))
+        {
+            HP = HP + 0.1f > 1.0f ? 1.0f : HP + 0.1f;
+        }
+        if (GUI.Button(HealthDown, "减血"))
+        {
+            HP = HP - 0.1f < 0.0f ? 0.0f : HP -0.1f;
+        }
+
+        showHP = Mathf.Lerp(showHP, HP,0.05f);
+        GUI.HorizontalScrollbar(HealthBar, 0.0f, showHP, 0.0f,1.0f);
+        HealthSlider.value = showHP;
+    }
+}
